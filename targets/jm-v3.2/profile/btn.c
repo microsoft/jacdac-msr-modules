0224967b2a788b944550e0db08a5faee4f552501
Michal Moskal <michal@moskal.me>
2021-03-19 20:12:46 -0700

    Add starfighter boards

---
+++ targets/jm-v3.2/profile/btn.c
@@ -0,0 +1,7 @@
+#include "jdprofile.h"
+
+FIRMWARE_IDENTIFIER(0x33da2759, "JM Btn v3.2");
+
+void app_init_services() {
+    btn_init(PIN_X0, 0, -1);
+}
---
+++ targets/jm-v3.2/profile/env3.c
@@ -0,0 +1,8 @@
+#include "jdprofile.h"
+
+FIRMWARE_IDENTIFIER(0x389761c7, "JM SHTC3 v3.2");
+
+void app_init_services() {
+    temp_init(shtc3_temperature);
+    humidity_init(shtc3_humidity);
+}
