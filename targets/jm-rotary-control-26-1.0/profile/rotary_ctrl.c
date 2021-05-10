cdcfb7a5f4f2504d9ade2bde7569a59cf7c72df5
James Devine <t-jamdev@microsoft.com>
2021-05-10 11:00:42 +0100

    add rotary control boards

---
+++ targets/jm-rotary-control-26-1.0/profile/rotary_ctrl.c
@@ -0,0 +1,8 @@
+#include "jdprofile.h"
+
+FIRMWARE_IDENTIFIER(0x3f39d51b, "JM Rotary Ctrl + Btn v1.0");
+
+void app_init_services() {
+    rotaryencoder_init(PA_7, PA_6, 20);
+    button_init(PA_0, 0, -1);
+}
