e0cbdc40276a90bf7d6829f904033a64be72e532
James Devine <t-jamdev@microsoft.com>
2021-06-07 22:07:57 +0100

    transform accelerometer data
    This may not be the best orientation, will find out...

--- targets/jm-accelerometer-30-0.2/profile/accelerometer.c
+++ targets/jm-accelerometer-30-0.2/profile/accelerometer.c
@@ -4,12 +4,12 @@ FIRMWARE_IDENTIFIER(0x33f9b086, "JM Accelerometer 30-0.2");
 
 // this depends on physical placement of accelerometer on PCB
 void accelerometer_data_transform(int32_t sample[3]) {
-    int32_t v0 = sample[0];
-    int32_t v1 = sample[1];
-    int32_t v2 = sample[2];
-    sample[0] = v1;
-    sample[1] = -v0;
-    sample[2] = -v2;
+    int32_t v0 = sample[0]; // x
+    int32_t v1 = sample[1]; // y
+    int32_t v2 = sample[2]; // z
+    sample[0] = -v0;
+    sample[1] = v1;
+    sample[2] = v2;
 }
 
 void app_init_services() {
