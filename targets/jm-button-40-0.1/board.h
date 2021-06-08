0ed57da1b0c5b6d40fc1fec52accda3407173a63
James Devine <t-jamdev@microsoft.com>
2021-06-08 18:20:03 +0100

    WIP: add G0 button module

---
+++ targets/jm-button-40-0.1/board.h
@@ -0,0 +1,23 @@
+#define PIN_LED NO_PIN
+#define PIN_LED_GND -1
+
+#define PIN_PWR NO_PIN
+#define PIN_P0 NO_PIN
+#define PIN_P1 NO_PIN
+
+#define UART_PIN PB_6
+#define UART_PIN_AF LL_GPIO_AF_0
+#define USART_IDX 1
+
+#define PIN_LED_R PA_13
+#define PIN_LED_G PB_8
+#define PIN_LED_B PB_0
+
+// calibrated to ~50lux per channel
+#define LED_R_MULT 250
+#define LED_G_MULT 76
+#define LED_B_MULT 221
+#define RGB_LED_PERIOD 600
+
+#define PIN_BL_LED PIN_LED_B
+#define PIN_BL_PERIOD 300
