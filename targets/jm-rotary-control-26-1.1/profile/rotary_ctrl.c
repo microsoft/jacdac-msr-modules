91d234239b1df1dd025bd4af4a59daf052234a12
James Devine <t-jamdev@microsoft.com>
2021-07-07 17:18:50 +0100

    add files for rotary controll 1.1

---
+++ targets/jm-rotary-control-26-1.1/profile/rotary_ctrl.c
@@ -0,0 +1,8 @@
+#include "jdprofile.h"
+
+FIRMWARE_IDENTIFIER(0x31736afd, "JM Rotary Ctrl + Btn v1.1");
+
+void app_init_services() {
+    rotaryencoder_init(PA_7, PA_6, 20);
+    button_init(PA_0, 0, -1);
+}
