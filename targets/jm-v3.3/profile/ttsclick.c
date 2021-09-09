6e14a560202c81380e1d59ca688cab2fd4103e23
James Devine <t-jamdev@microsoft.com>
2021-09-09 17:24:49 +0100

    move speech synth app to jm-v3.3

--- targets/jm-v3.3/profile/ttsclick.c
+++ targets/jm-v3.3/profile/ttsclick.c
@@ -1,6 +1,6 @@
 #include "jdprofile.h"
 
-FIRMWARE_IDENTIFIER(0x36426460, "JM Speech Synthesis v3.4");
+FIRMWARE_IDENTIFIER(0x37b3f787, "JM Speech Synthesis v3.3");
 
 void app_init_services() {
     speech_synthesis_init(&tts_click);
