9e3326291f079a8f1f8cedd94368e197d206d122
James Devine <t-jamdev@microsoft.com>
2021-09-16 22:03:12 +0100

    braille blinky...
    Commands were the wrong way round...

--- targets/jm-mikrobus-61-3.4/profile/braille.c
+++ targets/jm-mikrobus-61-3.4/profile/braille.c
@@ -20,13 +20,22 @@ uint8_t rxdata[255];
 const uint16_t rows[4] = {0x4200,0,0,0};
 
 #define RESET           0x8000
-#define ROW_0_LS_ON     0x4209
-#define COL_0_HS_ON     0x0081
-#define COL_0_HS_ON_P2  0xC001
 
-#define ROW_0_LS_OFF    0x4201
-#define COL_0_HS_OFF    0x0083
-#define COL_0_HS_OFF_P2  0x8001
+#define ROW_0_LS_ON     0x0942
+#define COL_0_HS_UP     0x8100
+#define COL_0_HS_UP_P2  0x01C0
+
+#define ROW_0_LS_OFF    0x0142
+#define COL_0_HS_DWN    0x8300
+#define COL_0_HS_DWN_P2  0x0180
+
+// #define ROW_0_LS_UP     0x4209
+// #define COL_0_HS_UP     0x0081
+// #define COL_0_HS_UP_P2  0xC001
+
+// #define ROW_0_LS_DWN    0x4201
+// #define COL_0_HS_DWN    0x0083
+// #define COL_0_HS_DWN_P2  0x8001
 
 
 void done_handler(void) {
@@ -36,12 +45,12 @@ void done_handler(void) {
 
 uint16_t reset = RESET;
 uint16_t row0En = ROW_0_LS_ON;
-uint16_t col0On = COL_0_HS_ON;
-uint16_t col0OnP2 = COL_0_HS_ON_P2;
+uint16_t col0On = COL_0_HS_UP;
+uint16_t col0OnP2 = COL_0_HS_UP_P2;
 
 uint16_t row0Dis = ROW_0_LS_OFF;
-uint16_t col0Off = COL_0_HS_OFF;
-uint16_t col0OffP2 = COL_0_HS_OFF_P2;
+uint16_t col0Off = COL_0_HS_DWN;
+uint16_t col0OffP2 = COL_0_HS_DWN_P2;
 
 
 void braille_send(void* data) {
