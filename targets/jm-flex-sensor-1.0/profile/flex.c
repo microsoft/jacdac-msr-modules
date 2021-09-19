8e62effb52454ae869a6e8c59dd63ef0fe34f018
James Devine <t-jamdev@microsoft.com>
2021-09-19 16:44:59 +0100

    swap from g030 to g031

--- targets/jm-flex-sensor-1.0/profile/flex.c
+++ targets/jm-flex-sensor-1.0/profile/flex.c
@@ -4,11 +4,5 @@
 FIRMWARE_IDENTIFIER(0x321e3946, "JM Flex v1.0");
 
 void app_init_services() {
-    while(1) {
-        int adc = adc_read_pin(PA_12);
-        (void) adc;
-        DMESG("adc %d ", adc);
-        target_wait_us(250000);
-    }
     potentiometer_init(NO_PIN, PA_12, NO_PIN);  // L, M, H
 }
