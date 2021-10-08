cbf7945019753ac6225ddb38d75f79d54dfdf0ea
James Devine <t-jamdev@microsoft.com>
2021-10-08 12:32:29 +0100

    add two profile for braille proto variants

--- targets/jm-mikrobus-61-3.4/profile/braille.c
+++
@@ -1,8 +0,0 @@
-#include "jdprofile.h"
-#include "jacdac/dist/c/dotmatrix.h"
-
-FIRMWARE_IDENTIFIER(0x3997beeb, "JM Braille v3.4");
-
-void app_init_services() {
-    braille_init(&ncv7726b, 4, 8, JD_DOT_MATRIX_VARIANT_BRAILLE);
-}
