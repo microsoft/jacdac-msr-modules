1542d117e51a4608e27216172992ac66b6390bd5
Michal Moskal <michal@moskal.me>
2021-11-23 10:24:46 -0800

    Add SHT30 module (64)

---
+++ targets/jm-v3.6/profile/env30.c
@@ -0,0 +1,8 @@
+#include "jdprofile.h"
+
+FIRMWARE_IDENTIFIER(0x30df7db3, "JM SHT30 64 v3.6");
+
+void app_init_services() {
+    thermometer_init(&temperature_sht30);
+    humidity_init(&humidity_sht30);
+}
