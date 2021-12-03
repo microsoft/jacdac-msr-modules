ff1cee1117c808a349ef06c5493c7e4fc7c49d7d
Michal Moskal <michal@moskal.me>
2021-12-03 15:59:14 -0800

    RPi bridge 3.7

--- targets/jm-spi-bridge-v3.7/profile/bridge.c
+++ targets/jm-spi-bridge-v3.7/profile/bridge.c
@@ -1,18 +1,16 @@
 #include "jdprofile.h"
 
-FIRMWARE_IDENTIFIER(0x30bc03c7, "JM SPI bridge v3.5A");
+FIRMWARE_IDENTIFIER(0x3495cc64, "JM SPI bridge v3.7");
 
 void bridge_init(void);
 
-/*
 static const power_config_t pcfg = {
     .pin_fault = PA_4,
     .pin_en = PC_15,
     .pin_pulse = NO_PIN,
 };
-*/
 
 void app_init_services() {
     bridge_init();
-   // power_init(&pcfg);
+    power_init(&pcfg);
 }
