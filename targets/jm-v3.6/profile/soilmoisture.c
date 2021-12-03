0d0bdfbb341c3927ab6720fe185f64dd768b5dd3
Michal Moskal <michal@moskal.me>
2021-12-02 18:51:23 -0800

    Use different cable for soil moisture

--- targets/jm-v3.6/profile/soilmoisture.c
+++ targets/jm-v3.6/profile/soilmoisture.c
@@ -3,16 +3,28 @@
 
 FIRMWARE_IDENTIFIER(0x37522d24, "JM Capacitive Soil Moisture v3.7");
 
+#if 0
+// for use with 1-1 2-2 3-3 cable
+#define PIN1 PIN_RX_CS
+#define PIN2 PIN_TX_MOSI
+#define PIN3 PIN_AN
+#else
+// for use with 1-3 2-2 3-1 cable
+#define PIN3 PIN_RX_CS
+#define PIN2 PIN_TX_MOSI
+#define PIN1 PIN_AN
+#endif
+
 void app_init_services() {
     ANALOG_SRV_DEF(JD_SERVICE_CLASS_SOIL_MOISTURE,                    //
-                   .pinL = PIN_RX_CS, .pinH = NO_PIN, .pinM = PIN_AN, //
+                   .pinL = PIN1, .pinH = NO_PIN, .pinM = PIN3, //
                    .offset = 0x18000, .scale = -2000,                 //
                    .variant = JD_SOIL_MOISTURE_VARIANT_CAPACITIVE,    //
                    .sampling_ms = 1000 - 150, .sampling_delay = 150         //
     );
 
-    // pin_set(PIN_RX_CS, 0);
-    // pin_setup_output(PIN_RX_CS);
-    pin_set(PIN_TX_MOSI, 1);
-    pin_setup_output(PIN_TX_MOSI);
+    // pin_set(PIN1, 0);
+    // pin_setup_output(PIN1);
+    pin_set(PIN2, 1);
+    pin_setup_output(PIN2);
 }
