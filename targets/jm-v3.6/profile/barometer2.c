145aa715cd262c71df4a9ecf3930fae0783168ee
Michal Moskal <michal@moskal.me>
2022-01-04 19:11:14 +0100

    remove accidental junk code

--- targets/jm-v3.6/profile/barometer2.c
+++ targets/jm-v3.6/profile/barometer2.c
@@ -2,15 +2,7 @@
 
 FIRMWARE_IDENTIFIER(0x33d141ab, "JM Pressure MPL3115A2 72 v3.8");
 
-#include <math.h>
-
-extern double foo;
-
 void app_init_services() {
-    double f = foo;
-    f = pow(f,f);
-    DMESG("f=%d", f);
-
     barometer_init(&pressure_mpl3115a2);
     thermometer_init(&temperature_mpl3115a2);
 }
