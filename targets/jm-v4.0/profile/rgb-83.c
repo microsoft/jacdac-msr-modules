d4ff0f417a5b2fceb211b3941300bf6fc0d0adf6
Michal Moskal <michal@moskal.me>
2022-03-03 17:55:44 -0800

    use new leddisplay in rgb83

--- targets/jm-v4.0/profile/rgb-83.c
+++ targets/jm-v4.0/profile/rgb-83.c
@@ -4,5 +4,5 @@
 FIRMWARE_IDENTIFIER(0x3e9e0de1, "JM RGB-8 83 v4.0");
 
 void app_init_services() {
-    ledstrip_init(JD_LED_STRIP_LIGHT_TYPE_WS2812B_GRB, 8, 200, JD_LED_STRIP_VARIANT_RING);
+    leddisplay_init(JD_LED_STRIP_LIGHT_TYPE_WS2812B_GRB, 8, 50, JD_LED_STRIP_VARIANT_RING);
 }
