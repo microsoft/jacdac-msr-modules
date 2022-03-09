bdfd46dd7d82536c12b9c0806efebee631ee7e42
James Devine <t-jamdev@microsoft.com>
2022-03-09 10:50:25 +0000

    PR feedback

--- targets/swd-mux-test-74-0.1/profile/swd_multi.c
+++ targets/swd-mux-test-74-0.1/profile/swd_multi.c
@@ -3,6 +3,5 @@
 FIRMWARE_IDENTIFIER(0x312305ea, "SWD MULTIPLEX v0.1");
 
 void app_init_services() {
-    bulb_service_init(PA_0);
-    // ledpixel_init(JD_LED_PIXEL_LIGHT_TYPE_WS2812B_GRB, 8, 200, JD_LED_PIXEL_VARIANT_RING);
+    lightbulb_init(PA_0);
 }
