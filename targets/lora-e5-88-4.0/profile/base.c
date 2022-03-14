0aa1d4805c1b4f8cb66d90ec1ea08e59b686f522
Michal Moskal <michal@moskal.me>
2022-03-14 15:58:42 -0700

    set board id; update submodules

--- targets/lora-e5-88-4.0/profile/base.c
+++ targets/lora-e5-88-4.0/profile/base.c
@@ -1,7 +1,7 @@
 #include "jdprofile.h"
 #include "tinyhw.h"
 
-FIRMWARE_IDENTIFIER(0, "JM-Lora E5 88 v4.0");
+FIRMWARE_IDENTIFIER(0x30085c3f, "JM-Lora E5 88 v4.0");
 
 void app_init_services() {
     spiflash_init();
