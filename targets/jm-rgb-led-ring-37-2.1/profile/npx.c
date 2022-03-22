ab5975c2b041747f191b01c8d8ccde0c952ee58a
James Devine <t-jamdev@microsoft.com>
2022-03-22 14:59:38 +0000

    rgb ring: use display instead of strip

--- targets/jm-rgb-led-ring-37-2.1/profile/npx.c
+++ targets/jm-rgb-led-ring-37-2.1/profile/npx.c
@@ -1,8 +1,8 @@
 #include "jdprofile.h"
-#include "jacdac/dist/c/ledstrip.h"
+#include "jacdac/dist/c/leddisplay.h"
 
 FIRMWARE_IDENTIFIER(0x3027f977, "JM RGB LED RING v2.1");
 
 void app_init_services() {
-    ledstrip_init(JD_LED_STRIP_LIGHT_TYPE_WS2812B_GRB, 8, 200, JD_LED_STRIP_VARIANT_RING);
+    leddisplay_init(JD_LED_DISPLAY_LIGHT_TYPE_WS2812B_GRB, 8, 200, JD_LED_DISPLAY_VARIANT_RING);
 }
