83ab302f6cde787f35a20d79c769b94726ed78e7
Michal Moskal <michal@moskal.me>
2022-04-08 17:23:46 -0700

    rename board to match silk

--- targets/touch-sensor-1.0/profile/touch.c
+++ targets/touch-sensor-1.0/profile/touch.c
@@ -2,7 +2,7 @@
 #include "jacdac/dist/c/capacitivebutton.h"
 #include "jacdac-c/inc/interfaces/jd_hw.h"
 
-FIRMWARE_IDENTIFIER(0x303b04aa, "JM TOUCH SENSOR V1.0");
+FIRMWARE_IDENTIFIER(0x303b04aa, "Touch Sensor v1.0");
 
 void app_init_services() {
     multicaptouch_init(&captouch_cap1298, 8);
