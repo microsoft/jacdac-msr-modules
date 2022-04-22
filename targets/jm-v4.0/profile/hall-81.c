cc1f46d9ad1b5095e585d9bb42eb81231af33740
Michal Moskal <michal@moskal.me>
2022-04-22 15:37:02 -0700

    use new magnetic service

--- targets/jm-v4.0/profile/hall-81.c
+++ targets/jm-v4.0/profile/hall-81.c
@@ -1,26 +1,21 @@
 #include "jdprofile.h"
-#include "interfaces/jd_adc.h"
-#include "jacdac/dist/c/switch.h"
+#include "jacdac/dist/c/magneticfieldlevel.h"
 
 FIRMWARE_IDENTIFIER(0x36db2b55, "JM-Hall (magnetic switch) 81 v4.0");
 
-#define V_MID 0x8000
-#define V_THR_LOW 3000
-#define V_THR_HI 4000
-
-static uint8_t is_active(uint32_t *state) {
-    int v = adc_read_pin(PIN_AN) - V_MID;
-    if (v < 0)
-        v = -v;
-
-    int thr = *state ? V_THR_LOW : V_THR_HI;
-    *state = v > thr;
-    return *state;
-}
+static const magneticfieldlevel_cfg_t cfg = {
+    .pin = PIN_AN,
+    .variant = JD_MAGNETIC_FIELD_LEVEL_VARIANT_ANALOG_NS,
+    .inverted = 0,
+    .offset = 0,
+    .thr_south_on = -4000,
+    .thr_south_off = -3000,
+    .thr_north_off = 3000,
+    .thr_north_on = 4000,
+};
 
 void app_init_services() {
     pin_set(PIN_RST, 1);
     pin_setup_output(PIN_RST);
-    pin_setup_analog_input(PIN_AN);
-    switch_init(is_active, JD_SWITCH_VARIANT_MAGNETIC);
+    magneticfieldlevel_init(&cfg);
 }
