5472e8a4bb03d51d05412f99e062ea1dec8edf31
Michal Moskal <michal@moskal.me>
2022-04-26 10:22:27 -0700

    invert hall sensor (according to spec)

--- targets/jm-v4.0/profile/hall-81.c
+++ targets/jm-v4.0/profile/hall-81.c
@@ -6,7 +6,7 @@ FIRMWARE_IDENTIFIER(0x36db2b55, "JM-Hall (magnetic switch) 81 v4.0");
 static const magneticfieldlevel_cfg_t cfg = {
     .pin = PIN_AN,
     .variant = JD_MAGNETIC_FIELD_LEVEL_VARIANT_ANALOG_NS,
-    .inverted = 0,
+    .inverted = 1,
     .offset = 0,
     .thr_south_on = -4000,
     .thr_south_off = -3000,
