52a6cc4773b5b23d065b4e605739aa849e5fa92e
Michal Moskal <michal@moskal.me>
2022-06-02 13:53:10 -0700

    disable temp on 204

--- targets/jm-v4.0/profile/env-204.c
+++ targets/jm-v4.0/profile/env-204.c
@@ -2,6 +2,7 @@
 
 FIRMWARE_IDENTIFIER(0x32301611, "JM-Environment 204 v4.2");
 
+#if 0
 static bool has_sgp30;
 static uint32_t next_hum;
 
@@ -15,6 +16,7 @@ void app_process() {
         }
     }
 }
+#endif
 
 void app_init_services() {
     if (co2_scd40.is_present()) {
@@ -26,6 +28,7 @@ void app_init_services() {
         humidity_init(&humidity_scd40);
         eco2_init(&co2_scd40);
     } else {
+#if 0
         temperature_init(&temperature_sht30);
         humidity_init(&humidity_sht30);
 
@@ -34,5 +37,16 @@ void app_init_services() {
             eco2_init(&eco2_sgp30);
             tvoc_init(&tvoc_sgp30);
         }
+#else
+        // the sgp30 gets quite warm, so the temp readings are not very good
+        // we thus don't report them and disable humidity compensation
+        if (eco2_sgp30.is_present()) {
+            eco2_init(&eco2_sgp30);
+            tvoc_init(&tvoc_sgp30);
+        } else {
+            temperature_init(&temperature_sht30);
+            humidity_init(&humidity_sht30);
+        }
+#endif
     }
 }
