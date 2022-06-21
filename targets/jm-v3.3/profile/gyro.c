39985dc0828f5acf2302528915bb87d25a1f9295
Michal Moskal <michal@moskal.me>
2022-06-21 11:39:29 -0700

    add gyro-215 use default gyro transform

--- targets/jm-v3.3/profile/gyro.c
+++ targets/jm-v3.3/profile/gyro.c
@@ -13,15 +13,6 @@ void accelerometer_data_transform(int32_t sample[3]) {
     sample[2] = -v2;
 }
 
-void gyroscope_data_transform(int32_t sample[3]) {
-    int32_t v0 = sample[0]; // x
-    int32_t v1 = sample[1]; // y
-    int32_t v2 = sample[2]; // z
-    sample[0] = v1;
-    sample[1] = -v0;
-    sample[2] = -v2;
-}
-
 void app_init_services() {
     // note that gyroscope must be *after* accelerometer due to gyroscope_pending variable processing
     accelerometer_init(&accelerometer_lsm6ds);
