8fd570ed2be6ed79ae38f714dfd238a6aa7e9b37
Michal Moskal <michal@moskal.me>
2022-11-30 13:04:04 -0800

    jacscript->devicescript rename

--- targets/lora-e5-88-4.0/profile/base.c
+++ targets/lora-e5-88-4.0/profile/base.c
@@ -2,14 +2,14 @@
 #include "tinyhw.h"
 
 #include "jd_client.h"
-#include "jacscript/jacscript.h"
+#include "devicescript/devicescript.h"
 
 FIRMWARE_IDENTIFIER(0x30085c3f, "JM-Lora E5 88 v4.0");
 
 #define PROG_SIZE JD_FLASH_SETTINGS_SIZE
 #define PROG_START (0x08000000 + (JD_FLASH_SIZE - BL_SIZE - PROG_SIZE))
 
-static const jacscriptmgr_cfg_t jacs_cfg = {
+static const devicescriptmgr_cfg_t devs_cfg = {
     .program_base = (void *)PROG_START,
     .max_program_size = PROG_SIZE,
 };
@@ -18,8 +18,8 @@ void app_init_services() {
     extcloud_init();
 
     jd_role_manager_init();
-    jacscriptmgr_init(&jacs_cfg);
+    devicescriptmgr_init(&devs_cfg);
 
-    jacscloud_init(&extcloud);
+    devscloud_init(&extcloud);
     tsagg_init(&extcloud);
 }
