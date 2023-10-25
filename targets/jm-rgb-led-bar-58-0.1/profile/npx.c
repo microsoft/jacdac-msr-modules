e0c473572b98b759c7327d60a693e038d2a7ed36
Juri Wolf <Amerlander@users.noreply.github.com>
2023-10-25 18:10:28 +0200

    Fix LED number for JM RGB LED BAR 10
    Set LED number from 8 to 10

--- targets/jm-rgb-led-bar-58-0.1/profile/npx.c
+++ targets/jm-rgb-led-bar-58-0.1/profile/npx.c
@@ -4,5 +4,5 @@
 FIRMWARE_IDENTIFIER(0x3e60b6fb, "JM RGB LED BAR 10 v0.1");
 
 void app_init_services() {
-    leddisplay_init(LIGHT_TYPE_WS2812B_GRB, 8, 200, JD_LED_VARIANT_STICK);
+    leddisplay_init(LIGHT_TYPE_WS2812B_GRB, 10, 200, JD_LED_VARIANT_STICK);
 }
